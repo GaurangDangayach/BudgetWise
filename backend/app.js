ac9fd7728ad60335d315fff78f47a9937f9792b5
GaurangDangayach <84135651+GaurangDangayach@users.noreply.github.com>
2026-06-21 07:28:26 +0530

    backend files

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const errorHandler = require('./middleware/errorHandler');
+const userRouter = require('./routes/userRouter');
+const transactionRouter = require('./routes/transactionRouter');
+
+const app = express();
+
+mongoose.connect("mongodb+srv://user1:_____@cluster1.ddwffa4.mongodb.net/Gaurang_BudgetWise")
+.then(() => console.log("DB Connected"))
+.catch((e) => console.log(e));
+
+app.use(cors({origin:["http://localhost:5173"]}));
+app.use(express.json());
+
+app.use("/api/v1/users",userRouter);
+app.use("/api/v1/transactions",transactionRouter);
+
+app.use(errorHandler);
+
+const port = 8000
+app.listen(port, () => console.log(`Serving on: http://localhost:${port}`))
